dcd7edbdea2f16f796b972cf4e79a32590f643aa
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-02-22 13:53:05 -0700

    Agregar archivos de proyecto.

---
+++ Polimorfismo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfismo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IFigura f1 = new Circulo { Radio = 5 };
+            IFigura f2 = new Rectangulo { Base = 5, Altura = 20 };
+            IFigura f3 = new Triangulo { Base = 15, Altura = 10 };
+
+            IFigura[] figuras = new IFigura[] { f1, f2, f3 };
+
+            foreach (var figura in figuras)
+            {
+                Console.WriteLine(figura.CalcularArea());
+            }
+            Console.ReadKey();
+        }
+    }
+}
